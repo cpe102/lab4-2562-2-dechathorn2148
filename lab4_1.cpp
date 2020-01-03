03be4760dbe9252ced0030ffc3c804a35b400be5
lab314-16@labeng.local <lab314-16@labeng.local>
2020-01-03 14:57:01 +0700

    lab4_1 Edit(1)

--- lab4_1.cpp
+++ lab4_1.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+double Calculated(int x)
+{
+    double y;
+    y = 3*pow(x,3)+2*2.71828+pow(2,2*x+1)+sqrt(pow(x,2)+1);
+    cout << "Result y = " << y;
+}
+
+int main()
+{
+    int x;
+    cout << "Enter X = ";
+    cin >> x;
+    Calculated(x);
+    return 0;
+}
 
